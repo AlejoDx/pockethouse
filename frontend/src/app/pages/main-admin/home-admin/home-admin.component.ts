6036b869291e7a306c85ca559d63e07bc8036ea4
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-10-30 13:39:25 -0300

    feat(client): User fetch service.

--- frontend/src/app/pages/main-admin/home-admin/home-admin.component.ts
+++ frontend/src/app/pages/main-admin/home-admin/home-admin.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HouseService } from 'src/app/pages/services/house.service'
+import { UserService } from 'src/app/pages/services/user.service'
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
   styleUrls: ['./home-admin.component.css']
 })
-export class HomeAdminComponent {
+export class HomeAdminComponent implements OnInit{
+
+  private houseService = inject(HouseService);
+  private userService = inject(UserService);
+  casas: any[] = [];
+  userDetails: any = "";
+  linkImagen: String="";
+
+  ngOnInit(): void {
+    this.houseService.list().subscribe((resp: any)=>{
+      console.log("Respuesta: " + resp[0].address)
+      this.casas = resp;
+    })
+    this.userService.authUserDetails().subscribe((resp: any)=>{
+      console.log("Respuesta: " + resp)
+      this.userDetails = resp;
+      this.linkImagen = "https://w7.pngwing.com/pngs/415/765/png-transparent-user-profile-linkedin-netwerk-money-order-fulfillment-round-face-saving-expert-moustache.png";
+    })
+  }
+
+  users : String[] = ["hola", "chau"];
+
+
 
 }
