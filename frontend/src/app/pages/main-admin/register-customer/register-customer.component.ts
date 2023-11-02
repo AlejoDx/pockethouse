666471241203ddf1e61274f1df8fe24c8f4acb9d
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-11-02 16:43:23 -0300

    feat(register): Connecting front-back register new client

--- frontend/src/app/pages/main-admin/register-customer/register-customer.component.ts
+++ frontend/src/app/pages/main-admin/register-customer/register-customer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
+import { DeviceService } from '../../services/device.service';
+import { EnvService } from '../../services/env.service';
 
 @Component({
   selector: 'app-register-customer',
@@ -15,6 +19,46 @@ export class RegisterCustomerComponent implements OnInit {
   public listEnviroments: any[] = [];
   public listDevice: any[] = [];
   public formRegister!: FormGroup;
+  userService = inject(UserService);
+  deviceService = inject(DeviceService);
+  envDervice = inject(EnvService);
+
+  client: any = {
+      "email": "alguien@gmail.com",
+      "image": "",
+      "name": "alguien",
+      "nameUser": "alguien",
+      "password": "123456",
+      "roles": [
+        "ROLE_USER"
+      ],
+      "topic": "string"
+  }
+
+  newDevice: any = {
+    "brand": "Jabius",
+    "description": "string",
+    "mqttClient": "client1",
+    "sn": "sn123456",
+    "subtopic": "string",
+    "type": {
+      "description": "SENSOR",
+      "id": 1,
+      "name": "SENSOR"
+    },
+    "unit": "C"
+  }
+
+  newEnvironment: any = {
+    "description": "string",
+    "devices": [],
+    "name": "Nombre",
+    "subTopic": "string"
+  }
+
+  createdUser: any = {};
+  public createdEnvironment: any[] = [];
+  public createdDevices: any[] = [];
 
   public stateForm = "0";
   months: string[] = [
@@ -25,8 +69,12 @@ export class RegisterCustomerComponent implements OnInit {
   private fb = inject(FormBuilder)
   ngOnInit(): void {
     this.formRegister = this.fb.group(
-
       {
+        formClientName:['', [Validators.required]],
+        formClientEmail:['', [Validators.required]],
+        formClientUser:['', [Validators.required]],
+        formClientPassword:['', [Validators.required]],
+        formClientTopic:['', [Validators.required]],
         descripcionHouse: ['', [Validators.required]],
         direccionHouse: ['', [Validators.required]],
         topicHouse: ['', [Validators.required]],
@@ -75,7 +123,37 @@ export class RegisterCustomerComponent implements OnInit {
     }
   }
   changeForm() {
+    this.client.name = this.formRegister.controls["formClientName"].value;
+    this.client.nameUser = this.formRegister.controls["formClientUser"].value;
+    this.client.email = this.formRegister.controls["formClientEmail"].value;
+    this.client.password = this.formRegister.controls["formClientPassword"].value;
+    this.client.topic = this.formRegister.controls["formClientTopic"].value;
+    // this.userService.addUser(this.client).subscribe((resp: any)=>{
+    //   console.log("Respuesta: " + resp.msg)
+    //   this.stateForm = "1";
+    // })
     this.stateForm = "1";
+
+  }
+
+  saveData(){
+    this.listDevice.forEach((device)=>{
+      this.newDevice.description = device.descripcionDevice;
+      this.newDevice.subtopic = device.topicDevice;
+      this.createdDevices.push(this.newDevice);
+      //   this.deviceService.addDevice(this.newDevice).subscribe((resp: any)=>{
+    //   this.createdDevices.push(resp);
+    // })
+    })
+    this.listEnviroments.forEach((env)=>{
+      this.newEnvironment.description = env.descripcionEnv;
+      this.newEnvironment.topic = env.topicEnv;
+      this.newEnvironment.devices = this.createdDevices;
+      this.envDervice.addEnv(this.newEnvironment).subscribe((resp:any)=>{
+        this.createdEnvironment.push(resp);
+        console.log(this.createdEnvironment);
+    })
+    })
 
   }
 }
